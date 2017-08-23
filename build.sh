1e327c34ade33f546a9682eade30f3a58e95066d
Ian Luites <IanLuites@users.noreply.github.com>
2017-08-23 06:44:56 -0400

    Update Phoenix node.

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ set -e
 
 ERLANG_VERSION=20.0.1
 ELIXIR_VERSION=1.5.1
+NODE_VERSION=8.4.0
+YARN_NODE_VERSION=latest
 
 # Generate
 mkdir -p build
@@ -11,11 +13,11 @@ mkdir -p build
 ## Base
 sed -e s/\${ERLANG_VERSION}/${ERLANG_VERSION}/ Dockerfile.erlang > build/Dockerfile.erlang
 sed -e s/\${ELIXIR_VERSION}/${ELIXIR_VERSION}/g Dockerfile.elixir | sed -e s/\${ERLANG_VERSION}/${ERLANG_VERSION}/ > build/Dockerfile.elixir
-sed -e s/\${ELIXIR_VERSION}/${ELIXIR_VERSION}/g Dockerfile.phoenix | sed -e s/\${ERLANG_VERSION}/${ERLANG_VERSION}/ > build/Dockerfile.phoenix
+sed -e s/\${ELIXIR_VERSION}/${ELIXIR_VERSION}/g Dockerfile.phoenix | sed -e s/\${ERLANG_VERSION}/${ERLANG_VERSION}/ | sed -e s/\${NODE_VERSION}/${NODE_VERSION}/g | sed -e s/\${YARN_NODE_VERSION}/${YARN_NODE_VERSION}/g > build/Dockerfile.phoenix
 
 ## Hipe
 sed -e s/\${ELIXIR_VERSION}/${ELIXIR_VERSION}/g Dockerfile.elixir-hipe | sed -e s/\${ERLANG_VERSION}/${ERLANG_VERSION}/ > build/Dockerfile.elixir-hipe
-sed -e s/\${ELIXIR_VERSION}/hipe-${ELIXIR_VERSION}/g Dockerfile.phoenix | sed -e s/\${ERLANG_VERSION}/${ERLANG_VERSION}/ > build/Dockerfile.phoenix-hipe
+sed -e s/\${ELIXIR_VERSION}/hipe-${ELIXIR_VERSION}/g Dockerfile.phoenix | sed -e s/\${ERLANG_VERSION}/${ERLANG_VERSION}/ | sed -e s/\${NODE_VERSION}/${NODE_VERSION}/g | sed -e s/\${YARN_NODE_VERSION}/${YARN_NODE_VERSION}/g > build/Dockerfile.phoenix-hipe
 
 # Build
 cd build
